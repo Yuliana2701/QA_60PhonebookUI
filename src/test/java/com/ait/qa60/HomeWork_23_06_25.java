8293d98e5d75454d3e63fd6c122edfa7ad6574ba
Shmahailo <yulifeya@ukr.netgit cleargit config --global user.email yulifeya@ukr.net>
2025-06-24 16:01:42 +0200

    Add homework

--- src/test/java/com/ait/qa60/HomeWork_23_06_25.java
+++ src/test/java/com/ait/qa60/HomeWork_23_06_25.java
@@ -1,5 +1,6 @@
 package com.ait.qa60;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -22,5 +23,36 @@ public class HomeWork_23_06_25 {
     @AfterMethod(enabled = false)
     public void testDown(){
         driver.quit();
+    }
+    @Test
+    public void findElementsByCssSelectorTest(){
+        //by id
+        driver.findElement(By.cssSelector("#small-searchterms"));
+        driver.findElement(By.cssSelector("#dialog-notifications-success"));
+
+        //by first img
+        driver.findElement(By.cssSelector("img"));
+
+        //by src
+        driver.findElement(By.cssSelector("img[src*='logo.png']"));
+        //find element by tag name -> ==css
+        driver.findElement(By.cssSelector("input"));
+
+        //by class name -> css (.)
+        driver.findElement(By.cssSelector(".header"));
+
+        // [attr = 'value']
+        driver.findElement(By.cssSelector("[name='q']"));
+
+        // contains -> *
+        driver.findElement(By.cssSelector("[href*='login']"));
+
+        //start -> ^
+        driver.findElement(By.cssSelector("[href^='/register']"));
+
+        // end on ->$
+        driver.findElement(By.cssSelector("[href$='/cart']"));
+
+
     }
 }
